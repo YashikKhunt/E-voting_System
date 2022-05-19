1ff479d6614da3b4ba16677961790e3c228ee8cb
Khunt Yashik <yashikkhunt2002@gmail.com>
2022-05-19 21:10:36 +0530

    Final Commit

---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+          Charusat University...
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
